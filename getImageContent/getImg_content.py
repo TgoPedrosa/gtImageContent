52e3b13e86922889dff8a517cb4939e17e310bc2
tgopedrosa <tgopedrosa@gmail.com>
2021-02-24 09:59:22 -0300

    Add function

--- getImageContent/getImg_content.py
+++ getImageContent/getImg_content.py
@@ -10,7 +10,11 @@ import argparse
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=False,
-	help="Imagem que será alinhada com o template")
+	help="Imagem que será aplicada a extração")
+
+ap.add_argument("-t", "--type", required=False,
+	help="Imagem que será aplicada a extração")
+
 args = vars(ap.parse_args())
 
 
@@ -52,5 +56,47 @@ def cropImage(img):
     cv2.waitKey(0)
 
 
+#Function in progress, need an improvement on parameters and edges
+def detectDocumentROIS(img):
+
+    rgb = img.copy()
+    gray = cv2.cvtColor(rgb, cv2.COLOR_BGR2GRAY)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 8))
+    grad = cv2.morphologyEx(gray, cv2.MORPH_GRADIENT, kernel)
+
+    _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9, 1))
+    connected = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+    # using RETR_EXTERNAL instead of RETR_CCOMP
+    contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    
+    mask = np.zeros(bw.shape, dtype=np.uint8)
+
+    for idx in range(len(contours)):
+        x, y, w, h = cv2.boundingRect(contours[idx])
+        mask[y:y+h, x:x+w] = 0
+        cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
+        r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
+
+        # parameters to draw the rectangle
+        if r > 0.45 and w > 120 and h > 30  and h < 170:
+            cv2.rectangle(rgb, (x, y), (x+w-1, y+h-1), (0, 255, 0), 2)
+            roi = rgb[y:y+h, x:x+w]
+            
+            #apply tesseract on rois
+            #text = pytesseract.image_to_string(roi,lang='por', config='--psm 6')
+            #print(text)
+
+    cv2.imshow("Matched Keypoints", connected)
+    cv2.waitKey(0)
+    cv2.imshow("Matched Keypoints", rgb)
+    cv2.waitKey(0)
+
 input_img = cv2.imread(args["image"])
-final_image = cropImage(input_img)  
+if args["type"] == 'roi':
+    final_image = detectDocumentROIS(input_img)  
+    
+else:
+    final_image = cropImage(input_img)  
